f4efebddd4d3ef36e9b1fe375b243f26c450c08c
Chad Aiena <caiena78@gmail.com>
2021-07-07 18:46:03 -0500

    added new files

--- get-mac-address-table.py
+++ get-mac-address-table.py
@@ -1,5 +1,6 @@
 import requests
-#from pprint import pprint
+import json
+from pprint import pprint
 import os
 
 
@@ -10,18 +11,13 @@ router = {"ip": os.getenv('switch_ip'), "port": "443", "user": os.getenv('switch
 headers = {"Accept": "application/yang-data+json",
            "Content-Type": "application/yang-data+json"}
 
-#[area-id=1]
-
-
-# list all vlan's
-#url = f"https://{router['ip']}:{router['port']}/restconf/data/Cisco-IOS-XE-wireless-client-oper:client-oper-data/common-oper-data=00:0e:8e:84:d3:9f"
-url = f"https://{router['ip']}:{router['port']}/restconf/data/Cisco-IOS-XE-wireless-client-oper:client-oper-data/dot11-oper-data=00:0e:8e:84:d3:9f"
-# print(url)
+url = f"https://{router['ip']}:{router['port']}/restconf/data/Cisco-IOS-XE-matm-oper:matm-oper-data"
+print(url)
 
 response = requests.get(url, headers=headers, auth=(
     router['user'], router['password']), verify=False)
 
 
 api_data = response.json()
-#print("/" * 50)
-print(api_data['Cisco-IOS-XE-wireless-client-oper:dot11-oper-data'])
+print("/" * 50)
+print(api_data)
