f4cb81286d96fa3cfeb6c1614e41d74024862ff9
Chad Aiena <caiena78@gmail.com>
2021-07-07 20:13:23 -0500

    updated interface enable to determin if the interface was already enabled

--- enable-interface.py
+++ enable-interface.py
@@ -14,10 +14,15 @@ headers = {"Accept": "application/yang-data+json",
            "Content-Type": "application/yang-data+json"}
 
 
-id="1%2F0%2F10" #1/0/10 %2f=/
+id="1%2F0%2F20" #1/0/20 %2f=/
 url = f"https://{router['ip']}:{router['port']}/restconf/data/Cisco-IOS-XE-native:native/interface/GigabitEthernet={id}/shutdown"    
 print(url)
 
+
+#you will get a 204 if it enabled the interface you get a 404 if its alread enabled
 response = requests.delete(url,headers=headers,auth=(router['user'], router['password']),verify=False)
 print(response)
-
+if response.status_code==204:
+    print("Interface was enabled")
+if response.status_code==404:
+    print("Interface was already enable")
